de45dce95c2ade6f8f6f2c0529c36392b61da3d0
controller-1 <90660794+controller-1@users.noreply.github.com>
2021-12-28 19:22:00 -0800

    Example with function and recursion

---
+++ Assignments/Unit 3/FunctionsandRecursion.c
@@ -0,0 +1,30 @@
+// Function and Recursion example
+
+#include <stdio.h>
+// 5! = 1*2*3*4*5
+// n! = 1*2*3*...*(n-1)*n
+//recursion : one function calls itself
+int factorial(int);
+int main(void) {
+    int n, facto;
+    printf("Please enter a positive integer: ");
+    scanf("%d",&n);
+    if(n<0){
+        printf("%d is negative! Aborting..\n", n);
+    }else{
+        facto = factorial(n);
+        printf("%d! = %d.\n", n , facto);
+    }
+    return 0;
+}
+int factorial(int n){
+    int result;
+    if(n==0){ // this special case acts as a stop for the recursion 
+        result=1;
+    }else{
+        result = n * factorial(n-1);
+    }
+    return result;
+}
+
+
